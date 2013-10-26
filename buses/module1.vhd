d2b8867c31411bfa7c10800328f421f285cb3c9e
cf16 <cf16rocket@gmail.com>
2013-10-26 20:33:08 +0200

    assign to std_logic_vector and operator &

--- buses/module1.vhd
+++ buses/module1.vhd
@@ -1,5 +1,4 @@
 ----------------------------------------------------------------------------------
--- Company: cf16
 -- Engineer: cf16
 -- Create Date:    07:45:59 10/24/2013 
 -- Module Name:    module1 - Behavioral 
@@ -9,8 +8,9 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.numeric_std.all;
 
 entity leds is
-      Port ( switches : in  STD_LOGIC_VECTOR(7 downto 0);
-             LED     :  out STD_LOGIC_VECTOR(7 downto 0));
+      Port ( switches : in  STD_LOGIC_VECTOR(4 downto 0);
+             LED     :  out STD_LOGIC_VECTOR(5 downto 0);
+				 LED2     :  out STD_LOGIC_VECTOR(1 downto 0));
 end leds;
 
 architecture Behavioral of leds is
@@ -18,6 +18,8 @@ begin
   --LED <= (3 downto 0 =>switches(3) AND switches(2) AND switches(1) AND switches(0),
   --7 downto 4 => switches(7) AND switches(6) AND switches(5) AND switches(4));
   --LED <= switches;
-  LED <= std_logic_vector(switches);-- & switches(1);
+  LED <= std_logic_vector(switches) & '1';-- & switches(1); LED(0) will be on
+                                          -- constantly
+  LED2 <= switches(0) & switches(1);
 end Behavioral;
 
