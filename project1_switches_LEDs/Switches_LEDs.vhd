2865c48354b1bd4c607bf9d0e6b68bbe5803fb12
cf16 <cf16rocket@gmail.com>
2013-10-27 13:17:17 +0100

    deploy

--- project1_switches_LEDs/Switches_LEDs.vhd
+++ project1_switches_LEDs/Switches_LEDs.vhd
@@ -3,9 +3,9 @@
 -- Create Date:    22:32:54 10/19/2013 
 -- Module Name:    Switches_LEDs - Behavioral 
 ----------------------------------------------------------------------------------
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
 
 entity Switches_LEDs is
     Port ( switch_0 : in  STD_LOGIC;
