3b03ae424f3b55eb0c4c7fdee7b987f8c5f3fd89
cf16 <cf16rocket@gmail.com>
2015-03-01 15:35:12 +0100

    constraints implementation change

--- project1_switches_LEDs/Switches_LEDs.vhd
+++ project1_switches_LEDs/Switches_LEDs.vhd
@@ -10,16 +10,16 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity Switches_LEDs is
     Port ( switch_0 : in  STD_LOGIC;
            switch_1 : in  STD_LOGIC;
-			  switch_6 : in  STD_LOGIC;
+			  switch_2 : in  STD_LOGIC;
            LED_0 : out  STD_LOGIC;
            LED_1 : out  STD_LOGIC;
-			  LED_6 : out  STD_LOGIC);
+			  LED_2 : out  STD_LOGIC);
 end Switches_LEDs;
 
 architecture Behavioral of Switches_LEDs is
 begin
   LED_0 <= switch_0;
   LED_1 <= switch_1;
-  LED_6 <= switch_6;
+  LED_2 <= switch_2;
 end Behavioral;
 
